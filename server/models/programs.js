6aff48ec48c585b1db3d8f1d86f2e2e663d96158
Bobbi Tarkany <bl.tarkany@gmail.com>
2024-07-30 16:47:30 -0700

    progrma model exportation

--- server/models/programs.js
+++ server/models/programs.js
@@ -1,59 +1,72 @@
 const { Schema, model } = require('mongoose');
 
-const programSchema = new Schema({
-  program_name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  program_type: {
-    type: String,
-    required: true,
-    enum: [
-      'strength',
-      'hypertrophy',
-      'endurance',
-      'power',
-      'weight_loss',
-      'weight_gain',
-      'maintenance',
-    ],
-  },
-  program_description: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  program_duration: {
-    type: Number,
-    required: true,
-  },
-  program_duration_unit: {
-    type: String,
-    default: 'weeks',
-    enum: ['days', 'weeks', 'months'],
-  },
-  program_level: {
-    type: String,
-    required: true,
-    enum: ['beginner', 'intermediate', 'advanced'],
-  },
-  program_workouts: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'workout',
+const programSchema = new Schema(
+  {
+    program_name: {
+      type: String,
+      required: true,
+      trim: true,
     },
-  ],
-  program_creator: {
-    type: Schema.Types.ObjectId,
-    ref: 'user',
+    program_type: {
+      type: String,
+      required: true,
+      enum: [
+        'strength',
+        'hypertrophy',
+        'endurance',
+        'power',
+        'weight_loss',
+        'weight_gain',
+        'maintenance',
+      ],
+    },
+    program_description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    program_duration: {
+      type: Number,
+      required: true,
+    },
+    program_duration_unit: {
+      type: String,
+      default: 'weeks',
+      enum: ['days', 'weeks', 'months'],
+    },
+    program_level: {
+      type: String,
+      required: true,
+      enum: ['beginner', 'intermediate', 'advanced'],
+    },
+    program_workouts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'workout',
+      },
+    ],
+    program_creator: {
+      type: Schema.Types.ObjectId,
+      ref: 'user',
+    },
+    program_created: {
+      type: Date,
+      default: Date.now,
+    },
+    program_updated: {
+      type: Date,
+      default: Date.now,
+    },
   },
-  program_created: {
-    type: Date,
-    default: Date.now,
-  },
-  program_updated: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  {
+    toJSON: {
+      getters: true,
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+const Program = model('program', programSchema);
+
+module.exports = Program;
